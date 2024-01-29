7c84f49d80633c0024cb0a13b3aa11fcc5042bbc
Perlar118 <perlamaria9545@gmail.com>
2024-01-29 03:04:08 -0700

    cask

--- Gastos.Android/MainActivity.cs
+++ Gastos.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Android.Views;
 
 namespace Gastos.Droid
 {
-    [Activity(Label = "Miniproy", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Miniproy", Icon = "@mipmap/iconn", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
